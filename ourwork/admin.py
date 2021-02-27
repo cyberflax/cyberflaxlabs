d1b8050e430a5d631413df9d366f8a7c9ea5ad3d
Ritik Sisodiya <ritik.s10120@gmail.com>
2021-02-27 17:36:40 +0530

    deploy

--- ourwork/admin.py
+++ ourwork/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import ourwork_cat,headbanner,Work
 # Register your models here.
-
-
+@admin.register(Work)
+class WorkAdmin(admin.ModelAdmin):
+    list_display = ['catagory','projectTitle','content']
 admin.site.register(ourwork_cat)
 admin.site.register(headbanner)
-admin.site.register(Work)
